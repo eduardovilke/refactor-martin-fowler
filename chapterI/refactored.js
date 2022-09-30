a52827d2d89e9d2cdf223b87582515ee54be6093
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-09-30 00:08:24 -0300

    refactor: extract function

--- chapterI/refactored.js
+++ chapterI/refactored.js
@@ -10,10 +10,7 @@ function statement (invoice, plays) {
     totalAmount += amountFor(perf)
   }
 
-  let volumeCredits = 0;
-  for(let perf of invoice.performances){
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  
 
   function amountFor(aPerformance){
     let result = 0;
@@ -54,6 +51,14 @@ function statement (invoice, plays) {
   }).format(aNumber/100);
   }
 
+  function totalVolumeCredits(){
+    let volumeCredits = 0;
+    for(let perf of invoice.performances){
+      volumeCredits += volumeCreditsFor(perf);
+    }
+  }
+
+  let volumeCredits = totalVolumeCredits()
   result += `Amount owed is ${usd(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
