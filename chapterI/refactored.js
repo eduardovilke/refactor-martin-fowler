198554cdc2cdfa28058d8bb3bfc3ea44413dc886
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-09-29 23:52:31 -0300

    refactor: isolated function

--- chapterI/refactored.js
+++ chapterI/refactored.js
@@ -10,12 +10,8 @@ function statement (invoice, plays) {
   }).format;
 
   for(let perf of invoice.performances) {    
-    //soma créditos por volume
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // soma um crédito extra para cada dez expectadores de comédia
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
-    // exibe a linha para esta requisição
     result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf)
   }
@@ -45,6 +41,14 @@ function statement (invoice, plays) {
   function playFor(aPerformance){
     return plays[aPerformance.playID]
   }
+
+  function volumeCreditsFor(perf){
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
