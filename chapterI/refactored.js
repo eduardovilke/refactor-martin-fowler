6739c41b2f6d99d9fd92f890e2dc4f31f754051d
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-09-30 00:07:10 -0300

    refactor: slide statements

--- chapterI/refactored.js
+++ chapterI/refactored.js
@@ -3,14 +3,14 @@ const playsMock = require('./plays.json');
 
 function statement (invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for(let perf of invoice.performances) {    
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf)
   }
-  
+
+  let volumeCredits = 0;
   for(let perf of invoice.performances){
     volumeCredits += volumeCreditsFor(perf);
   }
