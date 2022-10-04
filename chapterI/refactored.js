4d736e8a6f7905d460841f443331b7d7eb843be8
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-10-03 22:19:41 -0300

    refactor: remove unnecessary play

--- chapterI/refactored.js
+++ chapterI/refactored.js
@@ -7,7 +7,7 @@ function statement (invoice, plays) {
   statementData.performances = invoice.performances.map(enrichPerformance)
   statementData.totalAmount = totalAmount(statementData)
   statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-  return renderPlainText(statementData, plays);
+  return renderPlainText(statementData);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -67,7 +67,7 @@ function statement (invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
   for(let perf of data.performances) {
     result += ` ${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience} seats)\n`;
