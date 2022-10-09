6e5c72ff1a45906cada9b22c04ebcddf3b787e17
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-10-09 20:33:54 -0300

    refactor: replace conditional with polymorphism

--- src/chapterI/refactored/ComedyCalculator.js
+++ src/chapterI/refactored/ComedyCalculator.js
@@ -1,4 +1,12 @@
 import PerformanceCalculator from "./PerformanceCalculator.js";
 
 export default class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
